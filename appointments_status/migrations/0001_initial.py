2a5f81cf726c31c43904d5a23293e93079e749e9
Chris32cm <cristofersosa159@gmail.com>
2025-08-29 08:58:14 -0500

    incluir migraciones iniciales de los modelos

---
+++ appointments_status/migrations/0001_initial.py
@@ -0,0 +1,15 @@
+# patients_diagnoses/migrations/0001_initial.py
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        # sin dependencias, o agrega las que realmente necesites
+    ]
+
+    operations = [
+        # vacío: solo para satisfacer la dependencia
+    ]
